bee9056be136bc4219d792c0497f73712845c2f6
Sam Egger <segger7@tsn.at>
2024-09-26 13:41:07 +0200

    Alle vorgegebenen Klassen in das Projekt eingefügt
    Genre, Urheber, und Kommandozeilenmenü

---
+++ src/Kommandozeilenmenü.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Kommandozeilenmenü {
+
+Scanner scan;
+
+public Kommandozeilenmenü() {
+        this.scan = new Scanner(System.in);
+	}
+
+public void MenüAnzeigen() {
+    System.out.println("MEN�");
+    System.out.println("1) AUSGABE");
+    System.out.println("2) RECHNEN");
+    System.out.println("3) exit");
+}
+
+public void start() {
+    String s = "-";
+    while (!s.equals("3")) {
+        MenüAnzeigen();
+        s = scan.nextLine();
+        switch(s)
+        {
+            case "1":
+                this.ausgabe();
+                break;
+            case "2":
+                this.rechnen();
+                break;
+            case "3":
+                break;
+            default:
+                this.fehler();
+                break;
+        }
+    }
+    scan.close();
+}
+
+public void ausgabe()
+{
+    System.out.println("AUSGABE");
+}
+
+public void rechnen()
+{
+    System.out.println("RECHNEN 1+1 = 2");
+}
+
+public void fehler()
+{
+    System.out.println("Sie k�nnen nur 1,2 oder 3 eingeben!");
+}
+
+
+}
